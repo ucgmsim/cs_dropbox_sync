6f189c7c861965bf042c301d808485727c4672cb
joelridden <jriddenw@gmail.com>
2023-05-09 14:39:24 +1200

    basic run name reading

---
+++ cs_api/cs_api/__init__.py
@@ -0,0 +1,6 @@
+import server
+import app
+import custom_log_handler
+import api
+import utils
+import constants
---
+++ cs_api/cs_api/server.py
@@ -0,0 +1,29 @@
+import os
+import logging
+
+import flask
+
+from custom_log_handler import MultiProcessSafeTimedRotatingFileHandler
+
+
+app = flask.Flask("cs_api")
+
+logfile = os.path.join(os.path.dirname(__file__), "logs/logfile.log")
+os.makedirs(os.path.dirname(logfile), exist_ok=True)
+
+TRFhandler = MultiProcessSafeTimedRotatingFileHandler(filename=logfile, when="midnight")
+
+logging.basicConfig(
+    format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+    level=logging.DEBUG,
+    handlers=[TRFhandler],
+)
+
+TRFhandler.setLevel(logging.DEBUG)
+# To prevent having a same log twice
+app.logger.propagate = False
+app.logger.addHandler(TRFhandler)
+logging.getLogger("matplotlib").setLevel(logging.ERROR)
+
+# Add the endpoints
+from api import metadata
